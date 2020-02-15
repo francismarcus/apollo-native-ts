3d0c3d41bd013d75eebd47381030e8b50468e8b4
francismarcus <marcusmagnusson@outlook.com>
2020-02-15 21:21:00 +0100

    + styled components

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,65 @@
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
 import First from './components/First'
+import styled, { ThemeProvider } from 'styled-components/native'
 
 export default () => (
-    <View style={styles.container}>
-        <First />
-    </View>
+	<ThemeProvider theme={themes.light}>
+		<Container>
+			<StyledText> Hello </StyledText>
+		</Container>
+	</ThemeProvider>
 )
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
+const StyledText = styled.Text`
+	font-size: ${props => props.theme.fz.t1};
+	color: ${props => props.theme.color}
+`
+
+const Container = styled.View`
+	flex: 1;
+	background-color: ${({ theme }) => theme.bgColor};
+	align-items: center;
+	justify-content: center;
+`
+
+const fz = {
+	micro: "8px", // 8
+	small: "14px", // 18
+	regular: "17px", // 22
+	large: "19px", // 24
+	t3: "24px", // 28
+	t2: "32px", // 36
+	t1: "44px", // 56
+}
+const spacing = {
+	tiny: "8px",
+	small: "16px",
+	base: "24px",
+	large: "48px",
+	xlarge: "64px"
+}
+
+const shared = {
+	spacing,
+	fz
+}
+
+const themes = {
+	light: {
+		type: 'light',
+		bgColor: 'white',
+		color: '#ff5a5f',
+		primaryText: '#484848',
+		...shared
+	},
+
+	dark: {
+		type: 'dark',
+		bgColor: '#1F2933',
+		color: '#ff5a5f',
+		primaryText: 'white',
+		...shared
+
+	}
+};
+
---
+++ styled.d.ts
@@ -0,0 +1,29 @@
+// import original module declarations
+import 'styled-components';
+import { DefaultTheme } from 'styled-components/native'
+// and extend them!
+declare module 'styled-components' {
+	export interface DefaultTheme {
+		bgColor: string;
+		color: string;
+		type: string;
+		primaryText: string;
+		spacing: {
+			tiny: string;
+			small: string;
+			base: string;
+			large: string;
+			xlarge: string;
+		}
+		fz: {
+			micro: string,
+			small: string;
+			regular: string;
+			large: string;
+			t3: string;
+			t2: string;
+			t1: string;
+		}
+	
+	}
+}
